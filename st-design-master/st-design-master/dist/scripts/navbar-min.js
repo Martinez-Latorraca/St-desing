c87c2e40f9e5a121d2d6ed7f570a9c3fb3dd9db9
Nicolas Martinez <nicomar2004@gmail.com>
2021-09-16 15:39:59 -0300

    remove folders

--- st-design-master/st-design-master/dist/scripts/navbar-min.js
+++
@@ -1,1 +0,0 @@
-let prevScrollpos=window.pageYOffset;window.onscroll=function(){let e=window.pageYOffset,o=document.querySelector("header");o.style.top=prevScrollpos>e&&e>=0?"0":"-250px",prevScrollpos=e};
